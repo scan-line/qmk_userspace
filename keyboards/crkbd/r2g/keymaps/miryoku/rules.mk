aeead2e66979d460ba0597914acb218ca03221ac
scan-line <123326927+scan-line@users.noreply.github.com>
2024-09-07 13:31:15 +0100

    Enable my keyboards

---
+++ keyboards/crkbd/r2g/keymaps/miryoku/rules.mk
@@ -0,0 +1,14 @@
+# Copyright 2023 Scan Line
+# https://github.com/scan-line
+
+# Configure for limited AVR space.
+LTO_ENABLE = yes
+AVR_USE_MINIMAL_PRINTF = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+GRAVE_ESC_ENABLE = no 
+MAGIC_ENABLE = no
+SPACE_CADET_ENABLE = no
+
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
---
+++ keyboards/crkbd/rev1/keymaps/miryoku/rules.mk
@@ -0,0 +1,4 @@
+# Copyright 2023 Scan Line
+# https://github.com/scan-line
+
+CONVERT_TO = elite_pi
