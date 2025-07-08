655ce79274f6cb6319e9f14ce3f65045828a7f69
scan-line <123326927+scan-line@users.noreply.github.com>
2025-07-08 20:33:48 +0100

    Test 3

--- keyboards/mechboards/crkbd/rp2g/keymaps/miryoku/oled.c
+++ keyboards/mechboards/crkbd/rp2g/keymaps/miryoku/oled.c
@@ -5,8 +5,49 @@
 
 #include "oled.h"
 
+#include "qp.h"
+#include "qp_st77xx_opcodes.h"
+#include "qp_lvgl.h"
+#include "printf.h"
+
+
+void oled_draw_left(void) {
+}
+
+void oled_draw_right(void) {
+}
+
+void oled_init_styles(void) {
+}
 
 void oled_init(void) {
+    master = is_keyboard_master();
+    // transaction_register_rpc(SCR_SLV_SYNC, screen_sync_slave_handler);
+    // timer = // _read();
+    uprintf("Post Init\n");
+    wait_ms(LCD_WAIT_TIME);
+    // Initialise the LCD
+    lcd = qp_st7735_make_spi_device(LCD_WIDTH, LCD_HEIGHT, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, LCD_SPI_DIVISOR, LCD_SPI_MODE);
+    qp_init(lcd, LCD_ROTATION);
+
+    // Apply Offset
+    qp_set_viewport_offsets(lcd, LCD_OFFSET_X, LCD_OFFSET_Y);
+
+// Invert Colour
+#ifdef LCD_INVERT_COLOUR
+    qp_comms_start(lcd);
+    qp_comms_command(lcd, ST77XX_CMD_INVERT_ON);
+    qp_comms_stop(lcd);
+#endif
+
+    if (!qp_lvgl_attach(lcd)) return;
+
+    oled_init_styles();
+
+    if (is_keyboard_left())
+        oled_draw_left();
+    else
+        oled_draw_right();
 }
 
 void oled_housekeeping_task(void) {
